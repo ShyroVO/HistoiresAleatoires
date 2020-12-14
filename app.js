eebf6abcaac06593eae24f2a28dd497e98d1eb7f
ShyroVO <bosquet.laurie@outlook.com>
2020-12-14 16:37:31 +0100

    0.5

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let noms = ["Susu", "Sisi", "Marcko", "Polo"];
-let objets = ["bois", "pierre", "pain", "couteau"];
+let objets = ["bois", "pains", "sable", "poils"];
 let temperatures = ["2°", "10°", "25°", "0°"];
 let lieux = ["forêts", "plage", "maison", "dehors"];
 let verbes = ["manger", "boire", "promener", "courir"];
@@ -15,7 +15,7 @@ creationStory.addEventListener('click', function story(){
     let randomLieux = Math.floor(Math.random()* 4);
     let randomVerbes = Math.floor(Math.random()* 4);
 
-    textGenerator.innerHTML = noms[randomNoms] + " " + objets[randomObjets] + " " +
-        temperatures[randomTemperatures] + " " + lieux[randomLieux] + " " + verbes[randomVerbes];
+    textGenerator.innerHTML += utilisateurName + " et " + noms[randomNoms] + " prennent du " + objets[randomObjets] + " le rechauffe à " +
+        temperatures[randomTemperatures] + " en " + lieux[randomLieux] + " pour aller " + verbes[randomVerbes] + ". <br>";
 
 })
